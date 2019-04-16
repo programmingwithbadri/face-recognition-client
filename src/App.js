0ad23ef98e3bf3330de4539a647882833c69e747
Badri <badri.py@outlook.com>
2019-04-16 21:46:29 +0530

    Add particle.js for styling purpose

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation'
 import './App.css';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+  }
+
 class App extends Component {
   render() {
     return (
         <div className="App">
-        <Navigation/>
-        <Logo />
-        <Rank />
-        <ImageLinkForm />
+          <Particles  params={{particlesOptions}} />
+          <Navigation/>
+          <Logo />
+          <Rank />
+          <ImageLinkForm />
       </div>
     );
   }
