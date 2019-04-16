cae19770c3494b18e8ffd79087732885fc04ed96
Badri <badri.py@outlook.com>
2019-04-16 21:35:00 +0530

    Add the component Rank with default values

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Navigation from './components/Navigation/Navigation'
 import './App.css';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Navigation/>
-      <Logo />
-      <ImageLinkForm />
+        <div className="App">
+        <Navigation/>
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
       </div>
     );
   }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return(
+        <div>
+            <div>
+                {`Badri, your current rank is ...`}
+            </div>
+            <div>
+                {`#1`}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
