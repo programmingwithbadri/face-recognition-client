cf102fe70f64ca02c41c5454867ab945ff5a8561
Badri <badri.py@outlook.com>
2019-04-20 12:58:23 +0530

    add the clarifai api to do face detection

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank'; 
+import Clarifai from 'clarifai';
 
+const app = new Clarifai.App({
+  apiKey: 'c4a6d17b099f4ae09a4488790df241d2'
+})
 const particlesOptions = {
   particles: {
     number:{
@@ -14,7 +18,6 @@ const particlesOptions = {
         enable:true,
         value_area:800
       }
-
     }
   }
 }
@@ -31,7 +34,14 @@ class App extends Component {
     }
 
     this.onButtonSubmit = () => {
-      console.log('clicked');
+      app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
     }
   }
   render() {
