395f8e5bbb9eee1f122eae61583809ee4c900647
Badri <badri.py@outlook.com>
2019-04-24 23:36:02 +0530

    Add signin component along with navigation

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation'
 import './App.css';
 import Logo from './components/Logo/Logo';
+import SignIn from './components/SignIn/SignIn';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Rank from './components/Rank/Rank'; 
+import Rank from './components/Rank/Rank';
 import Clarifai from 'clarifai';
 
 const app = new Clarifai.App({
@@ -14,69 +15,74 @@ const app = new Clarifai.App({
 
 const particlesOptions = {
   particles: {
-    number:{
+    number: {
       value: 200,
-      density:{
-        enable:true,
-        value_area:800
+      density: {
+        enable: true,
+        value_area: 800
       }
     }
   }
 }
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-        input: '',
-        imageUrl: '',
-        box: {}
-      };
-    }
-    onInputChange = (event) =>{
-      this.setState({input: event.target.value});
-    }
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signIn'
+    };
+  }
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
 
-    onButtonSubmit = () => {
-      this.setState({imageUrl: this.state.input});
-      app.models
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response => {
-        this.displayFaceBox(this.calculateFaceLocation(response)
-      );
-    console.log(response.outputs[0].data.regions[0].region_info.bounding_box);}
-      )
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
       .catch(err => console.log(err))
-    }
+  }
 
-    calculateFaceLocation = (data) => {
-      const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-      const image = document.getElementById('inputImage');
-      const width = Number(image.width);
-      const height = Number(image.height);
-      console.log(height, width);
-      return{
-        leftCol: clarifaiFace.left_col * width,
-        topRow: clarifaiFace.top_row * height,
-        rightCol: width - (clarifaiFace.right_col * width),
-        bottomRow: height - (clarifaiFace.bottom_row * height)
-      }
-    }
+  onRouteChange = (route) => {
+    this.setState({ route });
+  }
 
-    displayFaceBox = (box) => {
-      console.log(box);
-      this.setState({box: box});
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  }
 
   render() {
     return (
-        <div className="App">
-        <Particles className="particles" params ={particlesOptions} />
-          <Navigation/>
-          <Logo />
-          <Rank />
-          <ImageLinkForm onButtonSubmit = {this.onButtonSubmit} onInputChange = {this.onInputChange}/>
-          <FaceRecognition box = {this.state.box} imageUrl={this.state.imageUrl}/>
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation onRouteChange={this.onRouteChange} />
+        {
+          this.state.route === 'signIn'
+            ? <SignIn onRouteChange={this.onRouteChange} />
+            : <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm onButtonSubmit={this.onButtonSubmit} onInputChange={this.onInputChange} />
+              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+            </div>
+        }
       </div>
     );
   }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Navigation = () => {
-    return(
-        <nav style = {{display: 'flex', justifyContent: 'flex-end'}}>
-        <p className = "f3 dim black underline pa2 pointer">
-            Sign out
+const Navigation = ({ onRouteChange }) => {
+    return (
+        <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <p onClick={() => onRouteChange('signIn')} className="f3 dim black underline pa2 pointer">
+                Sign out
         </p>
         </nav>
     )
